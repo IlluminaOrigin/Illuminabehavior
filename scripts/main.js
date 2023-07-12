5be32d26c6a16e4dbb76e684dc8a75c6e12c3cbd
KarutaYT <tomatoy34@gmail.com>
2023-07-12 10:27:39 +0900

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -121,7 +121,7 @@ system.afterEvents.scriptEventReceive.subscribe(ev => {
                 mhp = `\n${c+str}§7${str2}`;
                 thp = Math.ceil(entityScore / 0.05);
                 if (thp <= 1) thp = 1
-                const health = entity.getComponent("attribute")
+                const health = entity.getComponent("health")
                 switch (entity.typeId) {
                     case "minecraft:armor_stand":
                         {
