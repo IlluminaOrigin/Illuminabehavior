f09cdbc3f3675e68dc460135c347d9420d501c06
KarutaYT <tomatoy34@gmail.com>
2023-07-17 14:13:49 +0900

    update guild.js

--- scripts/guild.js
+++ scripts/guild.js
@@ -12,8 +12,16 @@ export function GuildCreateForm(source){
 }
 
 export function GuildAdminForm(source){
-    const form = new UI.ModalFormData()
-
+    const form = new UI.ActionFormData()
+    form.title(`§l§0ギルド管理`)
+    form.body(`§l§f何をしますか？`)
+    form.button(`§l§0メンバー追加`)
+    form.button(`§l§0メンバー削除`)
+    form.button(`§l§0幹部追加`)
+    form.button(`§l§0幹部削除`)
+    form.button(`§l§0ギルド名変更`)
+    form.button(`§l§0オーナー変更`)
+    form.button(`§l§0ギルド削除`)
     form.show(source).then((rs)=>{
         if(rs.canceled) return;
         GuildCreate(source,rs.formValues[0])
