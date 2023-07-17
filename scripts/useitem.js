90736225f731dd87c2d9dca0fdaaba4d933c8dca
KarutaYT <tomatoy34@gmail.com>
2023-07-17 13:58:20 +0900

    update useitem.js a

--- scripts/useitem.js
+++ scripts/useitem.js
@@ -3,7 +3,6 @@ import { world } from "@minecraft/server";
 world.afterEvents.itemUse.subscribe((ev)=>{
     switch(ev.itemStack.typeId){
         case "karo:guildinvite":{
-            world.sendMessage(`a`)
             if(Number(ev.itemStack.getLore()[0]) == NaN || Number(ev.itemStack.getLore()[0]) == 0) return;
             world.scoreboard.getObjective(`playerguild`).setScore(ev.source,Number(ev.itemStack.getLore()[0]))
             ev.source.sendMessage(`§aギルドに加入しました。`)
