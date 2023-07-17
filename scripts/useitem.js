365e04e979de3a4d21fbf819714e3b23328da44d
KarutaYT <tomatoy34@gmail.com>
2023-07-17 14:42:28 +0900

    update guildadmin.json and useitem.js

--- scripts/useitem.js
+++ scripts/useitem.js
@@ -1,8 +1,7 @@
 import { world } from "@minecraft/server";
 import { GuildAdminForm,GuildCreateForm,GuildNameChangeForm,GuildDeleteForm } from "guild.js";
 
-world.afterEvents.itemStartUseOn.subscribe((ev)=>{
-    world.sendMessage(`b`)
+world.afterEvents.itemUse.subscribe((ev)=>{
     switch(ev.itemStack.typeId){
         case "karo:guildinvite":{
             if(Number(ev.itemStack.getLore()[0]) == NaN || Number(ev.itemStack.getLore()[0]) == 0) return;
