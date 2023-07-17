640f49baf72f4b529db6fe4d8c18f3f05bf569bb
KarutaYT <tomatoy34@gmail.com>
2023-07-17 12:17:30 +0900

    update guild.js

--- scripts/guild.js
+++ scripts/guild.js
@@ -1,16 +1,2 @@
-import { DynamicPropertiesDefinition, world } from "@minecraft/server"
-
-world.afterEvents.worldInitialize.subscribe((ev) => {
-    let guildAmount = new DynamicPropertiesDefinition()
-    guildAmount.defineNumber("guildAmount",0)
-    ev.propertyRegistry.registerWorldDynamicProperties(guildAmount);
-    for(let i = 1;i < world.getDynamicProperty("guildAmount") + 1;i++){
-        guildAmount.defineString(`guild${i}`)
-    }
-});
-
-
-
-let dp = 
-    dp.defineNumber("d");
-   
+import { world } from "@minecraft/server";
+import { GuildCreate,GuildDelete,GuildNameChange } from "functions.js";
