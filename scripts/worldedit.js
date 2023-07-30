bdcb0272df0d65655196fe3d2474dd9f7432309e
KarutaYT <tomatoy34@gmail.com>
2023-07-30 14:59:52 +0900

    update worldedit.js

--- scripts/worldedit.js
+++ scripts/worldedit.js
@@ -1,7 +1,9 @@
 import {  world , system } from "@minecraft/server";
-
+const startVector = new Map()
 world.afterEvents.blockBreak.subscribe((ev)=>{
-    ev.brokenBlockPermutation.clone()
-    //ev.dimension.fillBlocks(ev.block.location,ev.block.location,)
-    //ev.block.location
+    if(ev.player.getComponent(`inventory`).container.getItem(ev.player.selectedSlot).typeId === `karo:we_axe`){
+        ev.dimension.fillBlocks(ev.block.location,ev.block.location,ev.brokenBlockPermutation)
+        startVector(ev.player.name ,ev.block.location)
+        world.sendMessage(`${startVector(ev.player.name)}`)
+    }
 })
