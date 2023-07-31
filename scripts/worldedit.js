c00cb90c5c5845b3017663948620c05e55a37fb8
KarutaYT <tomatoy34@gmail.com>
2023-07-31 13:56:37 +0900

    update worldedit.js

--- scripts/worldedit.js
+++ scripts/worldedit.js
@@ -1,9 +1,9 @@
-import {  world , MinecraftBlockTypes } from "@minecraft/server";
+import {  world , MinecraftBlockTypes,BlockEvent, Block} from "@minecraft/server";
 let startVector = new Map()
 let endVector = new Map()
 let degrees = new Map()
 let featherBlock = new Map()
-const airBlock = world.getDimension(`overworld`).getBlock({x:0,y:0,z:0})
+const airBlock = world.getDimension(`overworld`).getBlock({x:world.getPlayers()[0].location.x ,y:320,z: world.getPlayers()[0].location.z})
 world.afterEvents.blockBreak.subscribe((ev)=>{
     if(typeof ev.player.getComponent(`inventory`).container.getItem(ev.player.selectedSlot) === 'undefined') return
     if(ev.player.getComponent(`inventory`).container.getItem(ev.player.selectedSlot).typeId === `karo:we_axe`){
