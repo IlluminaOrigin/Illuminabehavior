ef58e5aa27aa5315053dbaf77abd6ae03b203c1d
KarutaYT <tomatoy34@gmail.com>
2023-08-04 17:15:54 +0900

    update scriptevents.js

--- scripts/scriptevents.js
+++ scripts/scriptevents.js
@@ -183,8 +183,9 @@ system.runInterval((ev)=>{
 
 system.runInterval(()=>{
     for(const p of world.getPlayers({tags:[`hatu`]})) {
-        world.scoreboard.getObjective(`nowX`).setScore(p,Math.ceil(p.location.x / 10))
-        world.scoreboard.getObjective(`nowZ`).setScore(p,Math.ceil(p.location.z / 10))
+        
+            world.scoreboard.getObjective(`nowX`).setScore(p , Math.ceil(p.location.x * 0.31746031746))
+            world.scoreboard.getObjective(`nowZ`).setScore(p , Math.ceil(p.location.z * 0.31746031746))
     }
     let playersName = []
     for(let i = 0;i < world.getPlayers({tags:[`hatu`]}).length;i++){
