24b03912565921ce1bb789e597de5b0e04c0091b
KarutaYT <tomatoy34@gmail.com>
2023-08-08 14:29:08 +0900

    update scriptevents.js

--- scripts/scriptevents.js
+++ scripts/scriptevents.js
@@ -93,7 +93,6 @@ system.afterEvents.scriptEventReceive.subscribe(ev => {
 system.runInterval((ev)=>{
     for(const p of world.getPlayers({excludeGameModes: [`creative`,`spectator`]})){
         p.onScreenDisplay.setActionBar(`${p.getVelocity().x} ${p.getVelocity().y} ${p.getVelocity().z} `)
-        
         if(p.isFlying) world.sendMessage(`飛んでいる ${p.name}`)
         if( -15 > p.getVelocity().x || p.getVelocity().x > 15 || p.getVelocity().z > 15 || p.getVelocity().z < -15){ if(!p.getEffect(`speed`)) world.sendMessage(`テレポート ${p.name}`)} else if( -1.5 > p.getVelocity().x || p.getVelocity().x > 1.5 || p.getVelocity().z > 1.5 || p.getVelocity().z < -1.5) {if(!p.getEffect(`speed`)) world.sendMessage(`速度規定 ${p.name}`) } else if( p.getVelocity().y > 15 ) {if(!p.getEffect(`jump_boost`)) world.sendMessage(`フライ ${p.name}`)} else if( p.getVelocity().y > 2 ) {if(!p.getEffect(`jump_boost`)) world.sendMessage(`フライ ${p.name}`)}
 
