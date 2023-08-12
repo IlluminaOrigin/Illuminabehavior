fa76bcf50a2495b140cad9165eaea7de557dc2e3
KarutaYT <tomatoy34@gmail.com>
2023-08-13 08:41:48 +0900

    update rank.js, scriptevents.js and spawn.js

--- scripts/rank.js
+++ scripts/rank.js
@@ -4,26 +4,33 @@ import { world , DynamicPropertiesDefinition , system} from '@minecraft/server';
 world.afterEvents.worldInitialize.subscribe(({ propertyRegistry }) => {
   const property = new DynamicPropertiesDefinition();
   /* 文字列用 最大文字数: world = 1MB, entity = 128KB */
-  property.defineString('LvObject', 10000000, '{}');
+  property.defineString('lvObject', 1000000, '{}');
   /* worldに登録 */
   propertyRegistry.registerWorldDynamicProperties(property);
-});
+})
 
+let first = 0
 let lvObj = new Object
-lvObj = JSON.parse(world.getDynamicProperty('LvObject'))
+world.afterEvents.playerSpawn.subscribe((ev)=>{
+    if(ev.initialSpawn && first === 0) {
+        
+        lvObj = `${JSON.parse(world.getDynamicProperty('lvObject'))}`
+        first = 1
+    }    
+})
 
 system.runInterval(()=>{
+    if(!world.getDimension(`overworld`).getEntities({tags: [`LvRank`]})[0]) return;
     for(const p of world.getAllPlayers()){
-        if(typeof world.scoreboard.getObjective(`lv`).getScore(p) === 'undefined') continue;
-        let playerLv= {[p.name]: world.scoreboard.getObjective(`lv`).getScore(p)}
+        let playerLv= {[p.name]: p.level}
         lvObj = Object.assign({}, lvObj, playerLv);
     }
-    world.setDynamicProperty(`LvObject`,JSON.stringify(lvObj))
+    world.setDynamicProperty(`lvObject`,`${JSON.stringify(lvObj)}`)
     const dataArray = Object.entries(lvObj).sort((a, b) => b[1] - a[1]);
 
     let rankName = `§6Lvランキング`
     // 上位10個を抽出して整形して表示
-    for (let i = 0; i < Math.min(dataArray.length, 10); i++) {
+    for (let i = 0; i < Math.min(Object.keys(lvObj).length, 10); i++) {
         const entry = dataArray[i];
         rankName += `\n§a${i + 1}位 §r${entry[0]} §r: ${entry[1]}`
     }
