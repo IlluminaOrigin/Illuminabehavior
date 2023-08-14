08c1803b52cb6494df84c1836a0d1c37dfb5dd00
KarutaYT <tomatoy34@gmail.com>
2023-08-14 15:36:04 +0900

    update 3 files and copy 1 file

--- scripts/scriptevents.js
+++ scripts/scriptevents.js
@@ -93,6 +93,7 @@ system.afterEvents.scriptEventReceive.subscribe(ev => {
             //sourceEntity
             if(coolTime.get(sourceEntity.name) === 0) {
                 world.sendMessage(`Left Click`)
+                //sourceEntity.triggerEvent(`karo:tanken`)
                 coolTime.set(sourceEntity.name,20)
             }
         }
